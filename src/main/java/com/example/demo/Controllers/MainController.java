95c3e7253589c1ca909226717e7056f7abe7fa0f
Aryan Verma <vermaaryanverma30@gmail.com>
2025-04-01 22:00:33 +0530

    removed unused code

--- src/main/java/com/example/demo/Controllers/MainController.java
+++ src/main/java/com/example/demo/Controllers/MainController.java
@@ -9,10 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.Jwt.JwtUtil;
 import com.example.demo.Services.TaskService;
 import com.example.demo.Services.UserService;
 import com.example.demo.Types.TaskPriority;
@@ -28,8 +26,6 @@ import java.util.List;
 @RestController
 public class MainController {
 
-    @Autowired
-    private JwtUtil jwtUtil;
 
     @Autowired
     private UserService userService;
@@ -37,27 +33,6 @@ public class MainController {
     @Autowired
     private TaskService taskService;
     
-    @GetMapping("/test-jwt")
-    public ResponseEntity<String> tst(@RequestHeader("Authorization") String token){
-
-        try{
-             token = token.substring(7);
-             System.out.println("Auth TOken = "+token);
-             Boolean isValid = jwtUtil.validateToken(token);
-
-             if(isValid){
-                String username = jwtUtil.extractUsername(token);
-
-                return new ResponseEntity<>("Authorized : "+username,HttpStatus.OK);
-             }
-        }catch(Exception e){
-            return new ResponseEntity<>("Exception Occured",HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-       
-
-        return new ResponseEntity<>("Un-authorized",HttpStatus.UNAUTHORIZED);
-    }
 
     @PostMapping("/create-task")
     public ResponseEntity<String> createTask(@RequestBody Task task){
